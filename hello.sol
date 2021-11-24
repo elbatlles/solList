a25e443c5ae61f1ad28c8949064664628b7bbc79
elbatlles <angelbatlles@gmail.com>
2021-11-24 13:52:45 +0100

    Create hello.sol
    Hello

---
+++ hello.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Hello {
+
+    uint256 number;
+
+  
+    function store() public view returns ( string memory) {
+       return "Hola";
+    }
+
+  }
